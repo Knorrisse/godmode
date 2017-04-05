a3bd7ea5cc43729f2470088cb57f9f98d196f41d
nikolas <a1600513@myy.haaga-helia.fi>
2017-04-05 13:35:27 +0000

    fix typo

--- modules/yodawg/manifests/init.pp
+++ modules/yodawg/manifests/init.pp
@@ -9,6 +9,6 @@ class yodawg {
 	file {'/var/wwww/html/index.html':
 	
 		require => Package{'apache2'},
-		conent => 'Yo I put a dawg in your dawg so you can dawg while yo dawg',
+		content => 'Yo I put a dawg in your dawg so you can dawg while yo dawg',
 	}
 }
